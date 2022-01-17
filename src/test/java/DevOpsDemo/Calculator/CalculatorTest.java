1074a51f0595b86058f703b4ab9a583e220c3045
pratisin <68809592+pratisin@users.noreply.github.com>
2022-01-17 19:05:30 +0530

    Update CalculatorTest.java

--- src/test/java/DevOpsDemo/Calculator/CalculatorTest.java
+++ src/test/java/DevOpsDemo/Calculator/CalculatorTest.java
@@ -10,28 +10,28 @@ public class CalculatorTest {
 	public void sumTest() {
 		//This test will check the sum functionality of the test
 		
-		assertTrue("Sum function not working fine.", calculator.sum(10, 20) == 30);
+		assertTrue("Sum function not working fine.", calculator.sum(10, 20) == 130);
 	}
 	
 	@Test
 	public void subtractTest() {
 		//This test will check the subtract functionality of the test
 		
-		assertTrue("Sum function not working fine.", calculator.subtract(100, 20) == 80);
+		assertTrue("Sum function not working fine.", calculator.subtract(100, 20) == 180);
 	}
 	
 	@Test
 	public void multiplyTest() {
 		//This test will check the multiply functionality of the test
 		
-		assertTrue("Sum function not working fine.", calculator.multiply(10, 20) == 200);
+		assertTrue("Sum function not working fine.", calculator.multiply(10, 20) == 1200);
 	}
 	
 	@Test
 	public void devideTest() throws Exception {
 		//This test will check the multiply functionality of the test
 		
-		assertTrue("Sum function not working fine.", calculator.divide(20, 10) == 2);
+		assertTrue("Sum function not working fine.", calculator.divide(20, 10) == 12);
 	}
 	
 		@Test
